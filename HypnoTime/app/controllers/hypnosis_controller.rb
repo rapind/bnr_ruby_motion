5376e641ab2ae8568253a90f70388995b77d91c0
Dave Rapin <dave@rapin.com>
2012-08-20 12:19:26 -0400

    finished silver challenge for ch7

--- HypnoTime/app/controllers/hypnosis_controller.rb
+++ HypnoTime/app/controllers/hypnosis_controller.rb
@@ -12,6 +12,31 @@ class HypnosisController < UIViewController
     self.view = HypnosisView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
   end
 
+  layout do
+
+    @selector = subview(
+      UISegmentedControl.alloc.initWithItems(%w( Red Green Blue)),
+      left: 20, top: view.bounds.size.height - 160,
+      width: 280, height: 44)
+
+    # Handle the selection event.
+    @selector.when(UIControlEventValueChanged) do
+      case @selector.selectedSegmentIndex
+      when 0
+        color = UIColor.redColor
+      when 1
+        color = UIColor.greenColor
+      when 2
+        color = UIColor.blueColor
+      else
+        color = UIColor.lightGrayColor
+      end
+
+      self.view.circleColor = color
+    end
+
+  end
+
   def viewDidLoad
     super
     p "Hypnosis controller loaded it's view."
