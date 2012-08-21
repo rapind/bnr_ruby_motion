2e1d1a251891ed000d9f0fb8046cde37dbfc0724
Dave Rapin <dave@rapin.com>
2012-08-21 10:36:33 -0400

    added bronze challenge for ch8, but can't really test

--- HeavyRotation/app/app_delegate.rb
+++ HeavyRotation/app/app_delegate.rb
@@ -2,15 +2,22 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
-    # So much cleaner to use bubble-wrap for notifications.
-    App.notification_center.observe UIDeviceOrientationDidChangeNotification do |notification|
-      p "orientationChanged: #{notification.object.orientation}"
-    end
 
     @window.rootViewController = HeavyController.alloc.init
     @window.rootViewController.wantsFullScreenLayout = true
     @window.backgroundColor = UIColor.whiteColor
     @window.makeKeyAndVisible
+
+    # So much cleaner to use bubble-wrap for notifications.
+    App.notification_center.observe UIDeviceOrientationDidChangeNotification do |notification|
+      p "orientationChanged: #{notification.object.orientation}"
+    end
+
+    App.notification_center.observe UIDeviceProximityStateDidChangeNotification do |notification|
+      p "proximityChanged: #{notification.object.proximityState}"
+      @window.rootViewController.view.backgroundColor = notification.object.proximityState ? UIColor.darkGrayColor : UIColor.clearColor
+    end
+
     true
   end
 
